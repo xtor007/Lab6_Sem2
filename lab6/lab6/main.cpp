9968782620620e8e82f9995953ba7f86c9a8f356
Anatoliy <tolxpams@gmail.com>
2021-05-18 11:50:53 +0300

    add main.cpp

---
+++ lab6/lab6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab6
+//
+//  Created by Anatoliy Khramchenko on 18.05.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

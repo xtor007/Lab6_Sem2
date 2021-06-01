6cba5b669919c105718fc769edaed0f6f93afadf
Anatoliy <tolxpams@gmail.com>
2021-06-01 13:05:12 +0300

    non-working geometry

---
+++ lab6/Vec3.cpp
@@ -0,0 +1,8 @@
+//
+//  Vec3.cpp
+//  lab6
+//
+//  Created by Anatoliy Khramchenko on 28.05.2021.
+//
+
+#include "Vec3.hpp"
---
+++ lab6/Vec3.hpp
@@ -0,0 +1,13 @@
+//
+//  Vec3.hpp
+//  lab6
+//
+//  Created by Anatoliy Khramchenko on 28.05.2021.
+//
+
+#ifndef Vec3_hpp
+#define Vec3_hpp
+
+#include <stdio.h>
+
+#endif /* Vec3_hpp */
